65b819b70b550e08807a5f5abbed2b15b1a5a762
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-12 11:42:39 +0500

    create chat list and constact file

--- app/(root)/chats/page.jsx
+++ app/(root)/chats/page.jsx
@@ -1,10 +1,21 @@
-import Header from '@/app/components/Header'
-import React from 'react'
+import ChatList from "@/app/components/ChatList";
+import Contact from "@/app/components/Contact";
 
-export default function page() {
+const Chats = () => {
   return (
-    <div>
-        <Header />
+    <div className="main-container flex flex-col md:flex-row h-full">
+      {/* Sidebar ChatList */}
+      <div className="w-full md:w-1/3 lg:w-1/2 border-r border-gray-200">
+        <ChatList />
+      </div>
+
+      {/* Contact Section */}
+      <div className="hidden md:block w-full md:w-2/3 lg:w-1/2">
+        <Contact />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Chats;
+

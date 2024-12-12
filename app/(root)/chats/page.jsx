6fee7282d2fea60f0eca0c82afae886676a0069d
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-12 12:22:22 +0500

    create loader

--- app/(root)/chats/page.jsx
+++ app/(root)/chats/page.jsx
@@ -1,20 +1,20 @@
-import ChatList from "@/app/components/ChatList";
-import Contact from "@/app/components/Contact";
+import ChatList from "@/components/ChatList";
+import Contact from "@/components/Contact";
+
 
 const Chats = () => {
-  return (
-    <div className="main-container flex flex-col md:flex-row h-full">
-      {/* Sidebar ChatList */}
-      <div className="w-1/3 border-r border-gray-200">
-        <ChatList />
-      </div>
-
-      {/* Contact Section */}
-      <div className="w-2/3">
-        <Contact />
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-container flex flex-col md:flex-row h-full">
+            {/* Sidebar ChatList */}
+            <div className="w-1/3 border-r border-gray-200">
+                <ChatList />
+            </div>
+            {/* Contact Section */}
+            <div className="w-2/3">
+                <Contact />
+            </div>
+        </div>
+    );
 };
 
 export default Chats;
